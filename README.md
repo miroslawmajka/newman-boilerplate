9ea2fcee5e667b41026b1815f7b63128fa9a0702
miroslawmajka <go2mirek@gmail.com>
2020-03-08 21:02:18 +0000

    First step into makeshift newman load testing
    Added the collection and local dummy environment to the repository as
    they have no security details attached.

--- README.md
+++ README.md
@@ -2,12 +2,17 @@
 
 Circle CI Build Status: [![CircleCI](https://circleci.com/gh/miroslawmajka/newman-boilerplate.svg?style=svg)](https://circleci.com/gh/miroslawmajka/newman-boilerplate)
 
-Run newman tests:
+The project focuses on "Postman/Newman" testing of the API part of the [Sample Express App](https://www.npmjs.com/package/sample-express-app).
+
+Postman collection [web link](https://miroslawmajka.postman.co/collections/8965684-9e451885-a44f-40c1-b714-9703d6ff7285?version=latest&workspace=cdd8fa83-b4e5-406b-8561-abab423f0dd4).
+
+Run newman acceptance tests:
 ```shell script
 ./run-newman.sh ${POSTMAN_COLLECTION_ID}
 ```
 
-# TODO
+Run newman load tests:
+```shell script
+npm test
+```
 
-* Explain Postman link creation and where `POSTMAN_COLLECTION_ID` comes from
-* Expand to use JWT authentication with an API
---
+++ run-load-tests.js
@@ -0,0 +1,83 @@
+const newman = require('newman');
+
+const env = require('./envHandler');
+
+const collectionPath = `./${env.POSTMAN_COLLECTION_NAME}.postman_collection.json`;
+const environmentPath = `./${env.POSTMAN_ENVIRONMENT_NAME}.postman_environment.json`;
+
+const POSTMAN_COLLECTION = require(collectionPath);
+const POSTMAN_ENVIRONMENT = require(environmentPath);
+const PARALLEL_RUNS = env.PARALLEL_RUNS || 3;
+
+const loadTestRuns = [];
+
+for (let i = 0; i < PARALLEL_RUNS; i++) loadTestRuns.push(i);
+
+(async() => {
+    try {
+        const results = await Promise.all(loadTestRuns.map(i => runTestCollection(i)));
+
+        console.log(`Finished running ${results.length} concurrent collections`);
+
+        const failedRuns = results.filter(r => r.failures.length > 0);
+
+        const failCount = failedRuns.length;
+
+        if (failCount > 0)
+        {
+            const failPrefix = '[FAIL]';
+
+            console.warn(`${failPrefix} failed collection runs: ${failCount}`);
+
+            failedRuns.forEach(fr => {
+                console.warn(`${failPrefix} collection with failed tests: ${fr.colName}`);
+
+                fr.failures.forEach((f, i) => {
+                    console.warn(`${failPrefix} ${fr.colName} index ${i}:`);
+                    console.warn(`${failPrefix} ${fr.colName} request: ${f.parent.name} / ${f.source.name}`);
+                    console.warn(`${failPrefix} ${fr.colName} test: ${f.error.test}`);
+                    console.warn(`${failPrefix} ${fr.colName} error message: ${f.error.message}`);
+                });
+            });
+
+            process.exit(1);
+        } else
+        {
+            console.log('[SUCCESS] All collections passed')
+        }
+    } catch (err) {
+        console.error('Failed running load tests');
+        console.error(err);
+    }
+})();
+
+async function runTestCollection(runIndex) {
+    return new Promise((resolve, reject) => {
+        const colName = `COL-${runIndex}`;
+        const newmanConfig = {
+            collection: POSTMAN_COLLECTION,
+            environment: POSTMAN_ENVIRONMENT,
+            reporters: 'junit',
+            reporter: {
+                junit: {
+                    export: `./test-results/junit/junit-load-${colName}.xml`
+                }
+            }
+        };
+
+        console.log(`Running ${colName} collection...`);
+
+        newman.run(newmanConfig, (err, summary) => {
+            if (err) return reject(err);
+
+            const failures = summary.run.failures;
+
+            console.log(`Finsihed ${colName} collection run with ${failures.length} test failures`);
+
+            resolve({
+                colName,
+                failures
+            });
+        });
+    });
+}
